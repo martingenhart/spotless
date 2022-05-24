a6d6c452bb615d92693e48ea9934d6bed85881b6
Genhart Martin <S8016585@p-dom.allianz-suisse.ch>
2022-05-24 15:08:18 +0200

    Initial github desktop commit

---
+++ src/main/java/ch/allianz/dsp/spotless/SpotlessApplication.java
@@ -0,0 +1,21 @@
+/* ****************************************************************************
+ *
+ * COPYRIGHT (c) 2022 by Allianz-Suisse, Zürich, Switzerland.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Allianz-Suisse.
+ *
+ ******************************************************************************/
+package ch.allianz.dsp.spotless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotlessApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpotlessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/allianz/dsp/spotless/SpotlessApplicationTests.java
@@ -0,0 +1,21 @@
+/* ****************************************************************************
+ *
+ * COPYRIGHT (c) 2022 by Allianz-Suisse, Zürich, Switzerland.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Allianz-Suisse.
+ *
+ ******************************************************************************/
+package ch.allianz.dsp.spotless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlessApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
