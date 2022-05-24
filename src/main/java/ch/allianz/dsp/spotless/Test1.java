8dbe617ae396a839d6b8cbc94e479071910be566
s8016585 <s8016585@NB81864.dy.allianz-suisse.ch>
2022-05-24 15:14:50 +0200

    Test eclipse commit

---
+++ src/main/java/ch/allianz/dsp/spotless/Test1.java
@@ -0,0 +1,15 @@
+/* ****************************************************************************
+ *
+ * COPYRIGHT (c) 2022 by Allianz-Suisse, Zürich, Switzerland.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Allianz-Suisse.
+ *
+ ******************************************************************************/
+package ch.allianz.dsp.spotless;
+
+public class Test1 {
+
+    public Test1() {
+
+    }
+}
---
+++ src/main/java/ch/allianz/dsp/spotless/Test2.java
@@ -0,0 +1,14 @@
+/* ****************************************************************************
+ *
+ * COPYRIGHT (c) 2002-2020 by Allianz-Suisse, Zürich, Switzerland.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Allianz-Suisse.
+ *
+ ******************************************************************************/
+package ch.allianz.dsp.spotless;
+
+public class Test2 {
+
+public Test2() { }
+
+}
